2be2741fe57b145824b2b42b62cfca313a9f5e3b
parth tiwari <parthtiwari98@gmail.com>
2023-12-24 22:06:57 +0530

    parallax-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./global.scss";
 import Hero from "./components/hero/Hero";
 import Nav from "./components/nav/Nav";
+import Parallax from "./components/parallax/parallax";
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
         <Nav />
         <Hero />
       </section>
-      <section id="Services">Parallax</section>
-      <section>Services</section>
+      <section>
+        <Parallax type={"services"} />
+      </section>
+      <section id="Services">Services</section>
+      <section>
+        <Parallax type={"portfolio"} />
+      </section>
       <section id="Portfolio"></section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,89 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    y: 10,
+    opacity: 0,
+    transition: {
+      duration: 2,
+      repeat: "infinity",
+    },
+  },
+};
+const sliderTextVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-320%",
+    transition: {
+      duration: 25,
+      repeat: "infinity",
+      repeatType: "Mirror",
+    },
+  },
+};
 
 function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>PARTH TIWARI</motion.h2>
+          <div className="Title">
+            <motion.h1 variants={textVariants}>Frontend Developer,</motion.h1>
+            <h3>
+              passionate about translating ideas into visually appealing and
+              user-friendly websites. With a creative touch and precise coding,
+              I bring digital experiences to life. Let&apos;s turn your vision
+              into a standout online presence.
+            </h3>
+          </div>
+
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the latest works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+        <motion.div
+          className="slidingTextContainer"
+          variants={sliderTextVariants}
+          initial="initial"
+          animate="animate"
+        >
+          React Redux Javascript Node
+        </motion.div>
+        <div className="imageContainer">
+          <img src="/Homepage.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Hero;
